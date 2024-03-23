87e9a6d08c12761c7ff07e5d546b1061f64e10cd
Kosana66 <kosanapavlovic66@gmail.com>
2024-03-23 20:08:49 +0100

    Resiti zasto se pobednik meca ne smesta u listu cetvrtfinalista

--- src/paket1/SeasonTournament.java
+++ src/paket1/SeasonTournament.java
@@ -1,5 +1,6 @@
 package paket1;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class SeasonTournament extends Tournament {
 
@@ -8,17 +9,68 @@ public class SeasonTournament extends Tournament {
     private ArrayList<Player> semiFinalists;
     private ArrayList<Player> finalists;
             
-    public SeasonTournament (){
-        
+    public SeasonTournament(String tekst, ArrayList<Player> contestants){
+        super(tekst, contestants);
+        roundOf16 = contestants;
+        quaterFinalists = new ArrayList<Player>();
     }
     
     @Override
     public void play(){
+        super.setPlayable(true);
+        Random rng = new Random();
+        int randomNumber1, randomNumber2;
+        int numOfContestants = 15;
+        Match match;
+        
+        for(int i = 1; i <= 8; i++) {
+            
+            randomNumber1 = rng.nextInt(numOfContestants-0+1);
+            do {
+                randomNumber2 = rng.nextInt(numOfContestants-0+1);
+            } while(randomNumber1 == randomNumber2);
+            match = new Match(roundOf16.get(randomNumber1), roundOf16.get(randomNumber2), super.tourSurface, super.numOfSets);
+            // konverzija plejera da moze u listu
+            winner = match.playMatch();
+            System.out.println(winner.toString());
+            quaterFinalists.add(winner);
+            roundOf16.remove(randomNumber1);
+            roundOf16.remove(randomNumber2);
+            if(numOfContestants > 1)
+                numOfContestants -= 2;
+            
+            System.out.println("randomNumber1 : " + randomNumber1);
+            System.out.println("randomNumber2 : " + randomNumber2);
+            System.out.println("Pobednik : " );
+            System.out.println("\t" + quaterFinalists.get(i-1).toString());
+            System.out.println();
+        }
+        System.out.println("KRAJ TURNIRA");
         
     }
     
     public static void main(String args[]) {
-     
+        ArrayList<Player> contestants = new ArrayList<Player>();
+        contestants.add(new Player("1,Novak Djokovic,mentality,hard,0"));
+        contestants.add(new Player("2,Carlos Alcaraz,forehand,clay,0"));
+        contestants.add(new Player("3,Jannik Sinner,backhand,hard,0"));
+        contestants.add(new Player("4,Daniil Medvedev,serve,hard,0"));
+        contestants.add(new Player("5,Andrey Rublev,forehand,hard,0"));
+        contestants.add(new Player("6,Alexander Zverev,backhand,hard,0"));
+        contestants.add(new Player("7,Holger Rune,serve,clay,0"));
+        contestants.add(new Player("8,Hubert Hurkacz,serve,grass,0"));
+        contestants.add(new Player("9,Casper Ruud,forehand,clay,0"));
+        contestants.add(new Player("10,Alex de Minaur,forehand,grass,0"));
+        contestants.add(new Player("11,Stefanos Tsitsipas,backhand,grass,0"));
+        contestants.add(new Player("12,Taylor Fritz,serve,hard,0"));
+        contestants.add(new Player("13,Grigor Dimitrov,forehand,hard,0"));
+        contestants.add(new Player("14,Tommy Paul,forehand,clay,0"));
+        contestants.add(new Player("15,Karen Khachanov,serve,hard,0"));
+        contestants.add(new Player("16,Frances Tiafoe,serve,clay,0"));
+        
+        
+        SeasonTournament turnir1 = new SeasonTournament("Monte-Carlo Masters,clay,Masters1000", contestants);
+        turnir1.play();
     }
     
 }
--- src/paket1/Tournament.java
+++ src/paket1/Tournament.java
@@ -1,7 +1,7 @@
 package paket1;
 import java.util.ArrayList;
 
-public class Tournament {
+abstract public class Tournament {
 
     protected String tourName;
     protected String tourType;
@@ -10,14 +10,39 @@ public class Tournament {
     protected int numOfSets;
     protected ArrayList<Player> contestants;
             
-    public Tournament (){
-        
+    public Tournament (String tekst, ArrayList<Player> contestants){
+        String [] tokeni = tekst.split(",");
+        if(tokeni.length != 3){
+            System.out.println("Greska pri ocitavanju turnira");
+            System.exit(0);
+        }  
+        this.tourName = tokeni[0];
+        this.tourSurface = tokeni[1];
+        this.tourType = tokeni[2];
+        this.playable = false;
+        this.contestants = contestants;
+        if(this.tourType.equals("Grand Slam"))
+            numOfSets = 3;
+        else if(this.tourType.equals("Masters1000"))
+            numOfSets = 2;
+    }
+    
+    public boolean getPlayable(){
+        return playable;
+    }
+    
+    public ArrayList<Player> getContestants(){
+        return contestants;
+    }
+    
+    public void setPlayable(boolean isPlayable){
+        playable = isPlayable;
+    }
+    
+    public void setContestants(ArrayList<Player> contestans){
+        this.contestants = contestans;
     }
     
     abstract public void play();
-    
-    public static void main(String args[]) {
-     
-    }
     
 }

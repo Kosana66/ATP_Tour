f6c3c2f85a00a04085cc10d3ca27dd3fd5bc5807
Kosana66 <kosanapavlovic66@gmail.com>
2024-03-25 15:37:02 +0100

    Pocetak pisanja Championship klase

--- src/paket1/Championship.java
+++ src/paket1/Championship.java
@@ -1,5 +1,12 @@
 package paket1;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Championship {
 
@@ -14,10 +21,46 @@ public class Championship {
         
     }
     public void recoverPlayers() {
-        
+        for(Player p : players) {
+            if(p.getInjured() == true)
+                p.setInjured(false);
+        }
     }
     public void loadFiles() {
+	File fajl = new File("."+"players.txt");
+        String tmpLine;
+	if(fajl.exists()){
+            BufferedReader in = null;
+            try {
+                in = new BufferedReader(new FileReader(fajl));
+            } catch (FileNotFoundException ex) {
+                System.out.println("Fajl player.txt ne postoji na datoj lokaciji.");
+                System.exit(0);
+            }
+            try {
+                while((tmpLine = in.readLine()) != null)
+                    players.add(new Player(tmpLine));
+            } catch (IOException ex) {
+                Logger.getLogger(Championship.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
         
+        fajl = new File("."+"tournaments.txt");
+	if(fajl.exists()){
+            BufferedReader in = null;
+            try {
+                in = new BufferedReader(new FileReader(fajl));
+            } catch (FileNotFoundException ex) {
+                System.out.println("Fajl tournament.txt ne postoji na datoj lokaciji.");
+                System.exit(0);
+            }
+            try {
+                while((tmpLine = in.readLine()) != null)
+                    tournaments.add(new SeasonTournament(tmpLine, players));
+            } catch (IOException ex) {
+                Logger.getLogger(Championship.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
     public static void main(String args[]) {
        
